085b7397d2e8f183d3e741311515ed9ca4afdc49
shyamnepal <shyamnepal595@gmail.com>
2023-02-14 09:29:34 +0545

    Initial commit

---
+++ Coding-test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coding_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
